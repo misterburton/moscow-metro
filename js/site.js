e10d33acf31863b164b5e589fc11087dbfd3e25d
Burton Rast <burtonrast@gmail.com>
2016-08-12 18:13:37 -0700

    first commit

---
+++ js/site.js
@@ -0,0 +1,8 @@
+lightGallery(document.getElementById('lightgallery'), {
+    mode: 'lg-fade',
+    cssEasing: 'ease-in-out',
+    speed: 500,
+    preload: 2,
+    hideBarsDelay: 3000,
+    closable: false
+});
